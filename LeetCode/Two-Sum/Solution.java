c7596255153be2d69951046619f633f1e50aaff2
S U P R E M E <sandeshmajhi446@gmail.com>
2026-08-06 21:24:10 +0545

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.java
+++ LeetCode/Two-Sum/Solution.java
@@ -1,12 +1,15 @@
- for(var i =0;i<nums.length;i++){
-    var item = nums[i];   
-    for(var j=0; j < nums.length;j++){
-        var item2 = nums[j];
-        var sum = item + item2;
-        
-    if(sum === target ){
-      return [i,j];
-      
-    }
-  }
- }
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3       for(int j=0;j<nums.length;j++)
+4       {
+5        for(int i=j+1;i<nums.length;i++)
+6        {
+7            if(nums[j]+nums[i]==target)
+8            {
+9                return new int[]{j,i};
+10            }
+11        }
+12       }
+13      return new int[]{};
+14    }
+15}
