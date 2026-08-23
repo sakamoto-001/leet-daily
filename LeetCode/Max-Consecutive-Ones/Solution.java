7d1f37d88b4398016a2ff4c4f880c84b3e0ff348
S U P R E M E <sandeshmajhi446@gmail.com>
2026-08-23 13:20:11 +0545

    Add solution for Max Consecutive Ones

--- LeetCode/Max-Consecutive-Ones/Solution.java
+++ LeetCode/Max-Consecutive-Ones/Solution.java
@@ -1,17 +1,15 @@
 1class Solution {
 2    public int findMaxConsecutiveOnes(int[] nums) {
-3        int sum = 0;
-4        int max =0;
-5        for(int i = 0; i<nums.length;i++)
-6        {
-7           if(nums[i]==1)
-8           sum++;
-9           else if(nums[i]==0)
-10           {
-11            max=Math.max(max,sum);
-12            sum=0;
-13           }
-14        }
-15         return Math.max(max,sum);
-16    }
-17}
+3        int sum = 0,max =0;
+4        for(int i = 0; i<nums.length;i++)
+5        {
+6           if(nums[i]==1)
+7           sum++;
+8           else {
+9            max=Math.max(max,sum);
+10            sum=0;
+11           }
+12        }
+13         return Math.max(max,sum);
+14    }
+15}
