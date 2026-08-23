92ff0688e22a5f0d6ef6b56bf7384b87828dd7bb
S U P R E M E <sandeshmajhi446@gmail.com>
2026-08-23 14:04:47 +0545

    Add solution for Find Numbers with Even Number of Digits

--- LeetCode/Find-Numbers-with-Even-Number-of-Digits/Solution.java
+++ LeetCode/Find-Numbers-with-Even-Number-of-Digits/Solution.java
@@ -1,16 +1,20 @@
 1class Solution {
 2    public int findNumbers(int[] nums) {
-3        int ans=0;
+3        int ans =0;
 4        for(int i : nums)
-5        {   int temp =i;
-6            int count =0;
-7            while(temp>0){
-8                temp /=10;
-9                count++;
-10            }
-11            if(count%2==0)
-12            ans++;
-13        }
-14        return ans;
-15    }
-16}
+5        {
+6            int temp = i;
+7            int count = 0;
+8            while(temp>0)
+9            {
+10                temp/=10;
+11                count++;
+12            }
+13            if(count%2==0)
+14            {
+15                ans++;
+16            }
+17        }
+18        return ans;
+19    }
+20}
