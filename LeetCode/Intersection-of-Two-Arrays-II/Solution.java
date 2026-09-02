9b40b104b381f0f064cc5229fa8c8e8611e93d46
S U P R E M E <sandeshmajhi446@gmail.com>
2026-09-02 11:44:04 +0545

    Add solution for Intersection of Two Arrays II

--- LeetCode/Intersection-of-Two-Arrays-II/Solution.java
+++ LeetCode/Intersection-of-Two-Arrays-II/Solution.java
@@ -7,25 +7,26 @@
 7        Arrays.sort(nums1);
 8        Arrays.sort(nums2);
 9        int[] ans = new int[nums1.length];
-10        int k = 0, i = 0,j = 0;
-11        while(i < nums1.length && j < nums2.length)
-12        {
-13            if(nums1[i]==nums2[j])
-14            {
-15                ans[k]=nums1[i];
-16                k+=1;
-17                i+=1;
-18                j+=1;
-19            }
-20            else if(nums1[i]<nums2[j])
-21            {
-22                i+=1;
-23            }
-24            else
-25            {
-26                j+=1;
-27            }
-28        }
-29        return Arrays.copyOf(ans,k);
-30    }
-31}
+10        int k = 0;
+11        int i = 0,j = 0;
+12        while(i < nums1.length && j < nums2.length)
+13        {
+14            if(nums1[i]==nums2[j])
+15            {
+16                ans[k]=nums1[i];
+17                k+=1;
+18                i+=1;
+19                j+=1;
+20            }
+21            else if(nums1[i]<nums2[j])
+22            {
+23                i+=1;
+24            }
+25            else
+26            {
+27                j+=1;
+28            }
+29        }
+30        return Arrays.copyOf(ans,k);
+31    }
+32}
