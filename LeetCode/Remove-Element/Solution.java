6b377c428e2ea58f79e118e7ea2bbdcd8068eb55
S U P R E M E <sandeshmajhi446@gmail.com>
2026-09-07 13:58:39 +0545

    Add solution for Remove Element

--- LeetCode/Remove-Element/Solution.java
+++ LeetCode/Remove-Element/Solution.java
@@ -1,14 +1,19 @@
 1class Solution {
 2    public int removeElement(int[] nums, int val) {
-3      int j=0;
-4      for(int i=0;i<nums.length;i++)
-5      {
-6        if(nums[i]!=val)
-7        {
-8            nums[j]=nums[i];
-9            j++;
-10        }
-11      } 
-12      return j; 
-13    }
-14}
+3      int i=0;
+4      int n = nums.length;
+5      while(i<n)
+6      {
+7        if(nums[i] == val)
+8        {
+9            nums[i] = nums[n-1];
+10            n--;
+11        }
+12        else
+13        {
+14            i++;
+15        }
+16      }
+17      return n;
+18    }
+19}
