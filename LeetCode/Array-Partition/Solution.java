d8899d23e84846935cb1181ba92145b82e652481
S U P R E M E <sandeshmajhi446@gmail.com>
2026-09-11 16:47:38 +0545

    Add solution for Array Partition

--- LeetCode/Array-Partition/Solution.java
+++ LeetCode/Array-Partition/Solution.java
@@ -1,11 +1,24 @@
 1class Solution {
 2    public int arrayPairSum(int[] nums) {
-3        Arrays.sort(nums);
-4        int sum = 0;
-5        for(int i = 0; i < nums.length;i=i+2)
-6        {
-7          sum += nums[i];
-8        }
-9        return sum;
-10    }
-11}
+3       int[] freq = new int[20001];
+4       int sum = 0;
+5       for(int n : nums)
+6       {
+7          freq[n+10000]++;
+8       }
+9       boolean sign = true;
+10       for(int i = 0; i < 20001; i++)
+11       {
+12        while(freq[i]>0)
+13        {
+14            if(sign)
+15            {
+16                sum += i-10000;
+17            }
+18            sign = !sign;
+19            freq[i]--;
+20        }
+21       }
+22       return sum;
+23    }
+24}
