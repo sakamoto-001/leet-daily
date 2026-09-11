86b58bf3aab9bcd07c58c85e3021f85e5e2ba49a
S U P R E M E <sandeshmajhi446@gmail.com>
2026-09-12 00:20:39 +0545

    Add solution for Unique 3-Digit Even Numbers

--- LeetCode/Unique-3-Digit-Even-Numbers/Solution.java
+++ LeetCode/Unique-3-Digit-Even-Numbers/Solution.java
@@ -1,34 +1,29 @@
 1class Solution {
 2    public int totalNumbers(int[] digits) {
 3       int[] freq = new int [10];
-4       for(int n : digits)
-5       {
-6        freq[n]++;
-7       } 
-8       int count = 0;
-9       for(int i = 100; i < 1000; i = i+2)
-10       {
-11        int[] need = new int[10];
-12         int temp = i;
-13        need[temp%10]++;
-14        temp/= 10;
-15        need[temp%10]++;
-16        temp/=10;
-17        need[temp%10]++;
-18        boolean check = true;
-19        for(int k = 0; k < 10; k++)
-20        {
-21            if(need[k] > freq[k])
+4       for(int n : digits){
+5        freq[n]++; } 
+6       int count = 0;
+7       for(int i = 100; i < 1000; i = i+2)
+8       {
+9        int a = i/100;
+10        int b = (i/10)%10;
+11        int c = i%10;
+12        if(freq[a]==0 || freq[b]==0 || freq[c]==0)continue;
+13            if(a==b && b==c)
+14            {
+15                if(freq[a]<3)continue;
+16            }
+17            else if(a==b || a==c)
+18            {
+19              if(freq[a]<2)continue;
+20            }
+21            else if(b==c)
 22            {
-23                check = false;
-24                break;
-25            }
-26        }
-27         if(check)
-28            {
-29                count++;
-30            }
-31       }
-32       return count;
-33    }
-34}
+23                if(freq[b]<2)continue;
+24            }
+25            count++;
+26       }
+27       return count;
+28    }
+29}
